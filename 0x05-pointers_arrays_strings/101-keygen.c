6bad5e8728fc69fdd817c495305f6ebad6f10e42
dagm <dagmayalew489@gmail.com>
2023-08-21 16:33:18 -0700

    fixing a problem

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,18 +8,18 @@
  * Return: 0 Always.
  */
 int main() {
-  const char* chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  const int num_chars = 62;
-  char password[9] = {0};
-  
-  srand(time(0)); // seed random number generator with current time
-  
-  for (int i = 0; i < 8; i++) {
-    int rand_index = rand() % num_chars;
-    password[i] = chars[rand_index];
-  }
-  
-  printf("Generated password: %s\n", password);
-  
-  return 0;
+	const char* chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const int num_chars = 62;
+	char password[9] = {0};
+
+	srand(time(0)); // seed random number generator with current time
+
+	for (int i = 0; i < 8; i++) {
+		int rand_index = rand() % num_chars;
+		password[i] = chars[rand_index];
+	}
+
+	printf("Generated password: %s\n", password);
+
+	return 0;
 }
