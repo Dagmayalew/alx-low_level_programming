fcb82ce9880d9ff98ff01f2685e5aa605c33e7dd
dagm <dagmayalew489@gmail.com>
2023-08-24 18:45:55 -0700

    a function that concatenates two strings without using the standard library

--- 0-strcat.c
+++ 0-strcat.c
@@ -8,19 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *ret = dest;
+int i, j;
 
-    while (*dest) {
-        dest++;
-    }
-
-    while (*src) {
-        *dest++ = *src++;
-    }
-
-    *dest = '\0';
-
-    return ret;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
 }
 
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
 
+return (dest);
+}
