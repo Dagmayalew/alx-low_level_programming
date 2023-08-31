af903f899d2cddfecec6e9156b68b4cf9a65f7b4
dagm <dagmayalew489@gmail.com>
2023-08-31 15:39:36 -0700

    fixing a bug

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,6 +7,8 @@
  */
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+		return (-1);
 	return (_sqrt(n, 1));
 }
 
@@ -18,9 +20,7 @@ int _sqrt_recursion(int n)
  */
 int _sqrt(int n, int i)
 {
-	if (n < 0)
-		return (-1);
-	if ((i * i) > n)
+	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
