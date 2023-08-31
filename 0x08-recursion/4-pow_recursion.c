11f31d428a3daea0f5aebb20175555aa9d2282f9
dagm <dagmayalew489@gmail.com>
2023-08-31 15:29:14 -0700

    fixinig betty style code error

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 
 
 /**
- * _strlen_recursion - a function that returns the value of x raised to the power of y
+ * _pow_recursion - value of x raised to the power of y
  * @x: value
  * @y: power
  * Return: recursion
@@ -10,8 +10,8 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return -1;
+		return (-1);
 	if (y == 0)
-		return 1;
-	return x * _pow_recursion(x, y-1);
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
